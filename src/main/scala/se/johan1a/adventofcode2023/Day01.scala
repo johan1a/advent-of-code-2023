b78bdce021073874913ab7c3f6fe5a8cd8961c95
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-01 07:54:42 +0100

    day01 part 1

--- src/main/scala/se/johan1a/adventofcode2023/Day01.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day01.scala
@@ -3,7 +3,11 @@ package se.johan1a.adventofcode2023
 object Day01 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    input.map{ line =>
+      val first = line.toCharArray.find(_.isDigit).get
+      val last = line.reverse.toCharArray.find(_.isDigit).get
+      s"$first$last".toInt
+    }.sum
   }
 
   def part2(input: Seq[String]): Int = {
--- src/test/scala/se/johan1a/adventofcode2023/Day01Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day01Test.scala
@@ -4,8 +4,12 @@ import se.johan1a.adventofcode2023.TestInputUtil.getInput
 
 class Day01Test extends munit.FunSuite {
 
+  test("Part 1 test") {
+    assertEquals(Day01.part1(getInput("day01/test.txt")), 142)
+  }
+
   test("Part 1") {
-    assertEquals(Day01.part1(getInput("day01/input.txt")), -1)
+    assertEquals(Day01.part1(getInput("day01/input.txt")), 142)
   }
 
   test("Part 2") {
