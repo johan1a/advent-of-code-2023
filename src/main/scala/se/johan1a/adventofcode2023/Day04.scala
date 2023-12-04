4a420c46e49ae07f0a0afd797c1e56f67715da76
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-04 06:36:18 +0100

    day04 part 1

--- src/main/scala/se/johan1a/adventofcode2023/Day04.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day04.scala
@@ -3,10 +3,35 @@ package se.johan1a.adventofcode2023
 object Day04 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    input.map(parseLine).map(check).sum
   }
 
   def part2(input: Seq[String]): Int = {
     -1
   }
+
+  def parseLine(line: String): (Seq[Int], Seq[Int]) = {
+    val splitted = line.split(": ").last
+    println(splitted.toSeq)
+    val winningAndMine = splitted.split('|')
+    println(winningAndMine.toSeq)
+    val winning = winningAndMine.head.split(" ").filter(_.nonEmpty).map(_.trim.toInt).toSeq
+    println(winningAndMine.head.split(" ").toSeq)
+    val mine = winningAndMine.last.split(" ").filter(_.nonEmpty).map(_.trim.toInt).toSeq
+    (winning, mine)
+  }
+
+  def check(input: (Seq[Int], Seq[Int])): Int = {
+    input match {
+      case (winning: Seq[Int], mine: Seq[Int]) => {
+        val nbrWinning = winning.filter(mine.contains).size
+        if (nbrWinning > 0) {
+          Math.pow(2, nbrWinning - 1).toInt
+        } else {
+          0
+        }
+      }
+    }
+  }
+
 }
--- src/test/scala/se/johan1a/adventofcode2023/Day04Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day04Test.scala
@@ -5,7 +5,11 @@ import se.johan1a.adventofcode2023.TestInputUtil.getInput
 class Day04Test extends munit.FunSuite {
 
   test("Part 1") {
-    assertEquals(Day04.part1(getInput("day04/input.txt")), -1)
+    assertEquals(Day04.part1(getInput("day04/input.txt")), 21138)
+  }
+
+  test("Part 1 test") {
+    assertEquals(Day04.part1(getInput("day04/test.txt")), 13)
   }
 
   test("Part 2") {
