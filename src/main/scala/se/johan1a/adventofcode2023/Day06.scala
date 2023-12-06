11f84390a7cdcc380244de7119c47966beccdf0c
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-06 06:33:37 +0100

    day06 cleanup

--- src/main/scala/se/johan1a/adventofcode2023/Day06.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day06.scala
@@ -14,57 +14,54 @@ object Day06 {
 
   def part2(input: Seq[String]): Int = {
     val (duration, distance) = parse2(input)
-    println(s"duration $duration, distance $distance")
     check2(duration, distance).toInt
   }
 
   def check(duration: Long, record: Long): Long = {
     0.until(duration.toInt)
-      .map { minutesWaited =>
+      .filter { minutesWaited =>
         val speed = minutesWaited
         speed * (duration - minutesWaited)
       }
       .filter { result =>
         result > record
-      }.size
+      }
+      .size
   }
 
-  def check2(time: Long, distance: Long): Long = {
-    println("searching start")
-    val startOfWins = search(time, distance, true)
-    val endOfWins = search(time, distance, false)
-    println(s"startOfWins $startOfWins endOfWins $endOfWins")
+  def check2(duration: Long, distance: Long): Long = {
+    val startOfWins = search(duration, distance, true)
+    val endOfWins = search(duration, distance, false)
     endOfWins - startOfWins
   }
 
-  def search(time: Long, distance: Long, shouldWin: Boolean): Long = {
+  def search(duration: Long, distance: Long, shouldWin: Boolean): Long = {
     var l = 0L
-    var r = time
+    var r = duration
     var mid = -1L
     while (l <= r) {
       mid = l + (r - l) / 2L
 
-      val resultBefore = wins(time, distance, mid-1)
-      val result = wins(time, distance, mid)
+      val resultBefore = wins(duration, distance, mid - 1)
+      val result = wins(duration, distance, mid)
       if (result == shouldWin && resultBefore != shouldWin) {
         return mid
       } else if (result != shouldWin && resultBefore != shouldWin) {
         l = mid + 1
-      } else if (result == shouldWin && resultBefore == shouldWin){
+      } else if (result == shouldWin && resultBefore == shouldWin) {
         r = mid - 1
       }
     }
     -1
   }
 
-  def wins(time: Long, distance: Long, minutesWaited: Long): Boolean = {
-      val speed = minutesWaited
-      (speed * (time - minutesWaited)) > distance
+  def wins(duration: Long, distance: Long, minutesWaited: Long): Boolean = {
+    val speed = minutesWaited
+    (speed * (duration - minutesWaited)) > distance
   }
 
   def parse(lines: Seq[String]) = {
     (Utils.numbers(lines.head), Utils.numbers(lines.last))
-
   }
 
   def parse2(lines: Seq[String]) = {
