ef3834cf1ca24bc7a61f6d56c3eb20ec758df198
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-08 08:04:01 +0100

    day08 part 1

--- src/main/scala/se/johan1a/adventofcode2023/Day08.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day08.scala
@@ -1,12 +1,51 @@
 package se.johan1a.adventofcode2023
 
+import scala.collection.mutable.ArrayBuffer
+
 object Day08 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    val (instructions, nodes) = parse(input)
+    println(instructions)
+    println(nodes)
+    count(instructions, nodes, "AAA", 0, 0)
   }
 
   def part2(input: Seq[String]): Int = {
     -1
+  }
+
+  def count(
+      instructions: ArrayBuffer[String],
+      nodes: Map[(String, String), String],
+      pos: String,
+      stepsTaken: Int,
+      i: Int
+  ): Int = {
+    if (pos == "ZZZ") {
+      stepsTaken
+    } else {
+      val instruction = instructions(i)
+      val nextPos = nodes((pos, instruction))
+      count(instructions, nodes, nextPos, stepsTaken + 1, (i + 1) % instructions.size)
+    }
+  }
+
+  def parse(input: Seq[String]): (ArrayBuffer[String], Map[(String, String), String]) = {
+    val instructions = ArrayBuffer().appendAll(input.head.toArray).map(_.toString)
+    val nodes = input
+      .drop(2)
+      .flatMap { line =>
+        line match {
+          case s"$src = ($left, $right)" =>
+            Seq(
+              (src, "L") -> left,
+              (src, "R") -> right
+            )
+        }
+      }
+      .toMap
+
+    (instructions, nodes)
   }
 }
--- src/test/scala/se/johan1a/adventofcode2023/Day08Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day08Test.scala
@@ -4,8 +4,12 @@ import se.johan1a.adventofcode2023.TestInputUtil.getInput
 
 class Day08Test extends munit.FunSuite {
 
+  test("Part 1 test") {
+    assertEquals(Day08.part1(getInput("day08/test.txt")), 2)
+  }
+
   test("Part 1") {
-    assertEquals(Day08.part1(getInput("day08/input.txt")), -1)
+    assertEquals(Day08.part1(getInput("day08/input.txt")), 20513)
   }
 
   test("Part 2") {
