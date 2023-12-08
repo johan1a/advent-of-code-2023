354ae53e9ad3a796eb681b62ed9c673889afaa5f
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-08 08:36:56 +0100

    day08 part 2 naive approach

--- src/main/scala/se/johan1a/adventofcode2023/Day08.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day08.scala
@@ -1,20 +1,22 @@
 package se.johan1a.adventofcode2023
 
+import scala.annotation.tailrec
 import scala.collection.mutable.ArrayBuffer
 
 object Day08 {
 
   def part1(input: Seq[String]): Int = {
     val (instructions, nodes) = parse(input)
-    println(instructions)
-    println(nodes)
     count(instructions, nodes, "AAA", 0, 0)
   }
 
-  def part2(input: Seq[String]): Int = {
-    -1
+  def part2(input: Seq[String]): Long = {
+    val (instructions, nodes) = parse(input)
+    val startingPositions = nodes.keys.map(_._1).filter(_.endsWith("A")).toSet
+    count2(instructions, nodes, startingPositions, 0, 0)
   }
 
+  @tailrec
   def count(
       instructions: ArrayBuffer[String],
       nodes: Map[(String, String), String],
@@ -28,6 +30,31 @@ object Day08 {
       val instruction = instructions(i)
       val nextPos = nodes((pos, instruction))
       count(instructions, nodes, nextPos, stepsTaken + 1, (i + 1) % instructions.size)
+    }
+  }
+
+  @tailrec
+  def count2(
+      instructions: ArrayBuffer[String],
+      nodes: Map[(String, String), String],
+      currentPositions: Set[String],
+      stepsTaken: Long,
+      i: Int
+  ): Long = {
+    if (stepsTaken % 100000 == 0) {
+      println(stepsTaken)
+    }
+
+    if (currentPositions.forall(_.endsWith("Z"))) {
+      stepsTaken
+    } else {
+      val instruction = instructions(i)
+
+      val nextPositions = currentPositions.map { pos =>
+        nodes((pos, instruction))
+      }
+
+      count2(instructions, nodes, nextPositions, stepsTaken + 1, (i + 1) % instructions.size)
     }
   }
 
