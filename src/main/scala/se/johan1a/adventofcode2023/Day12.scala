0c304d1b3d5bcd68a24c6222a4e8282a4d0fe578
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-12 07:56:51 +0100

    day12 part2

--- src/main/scala/se/johan1a/adventofcode2023/Day12.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day12.scala
@@ -4,61 +4,123 @@ import scala.collection.mutable.ArrayBuffer
 
 object Day12 {
 
-  def part1(input: Seq[String]): Int = {
-    println(s"\n")
-    input.map(parse).map { case (line, nbrs) => nbrArrangements(line, nbrs, 0, 0) }.sum
+  def part1(input: Seq[String]): Long = {
+    input
+      .map(parse)
+      .map { case (line, nbrs) =>
+        cache = Map()
+        nbrArrangements(line, nbrs, 0, 0)
+      }
+      .sum
   }
 
-  def part2(input: Seq[String]): Int = {
-    -1
+  def part2(input: Seq[String]): Long = {
+    println(s"input size ${input.size}")
+    input
+      .map(parse)
+      .map(unfold)
+      .map { case (line, nbrs) =>
+        cache = Map()
+        val r = nbrArrangements(line, nbrs, 0, 0)
+        println(s"got $r")
+
+        r
+      }
+      .sum
   }
 
-  def nbrArrangements(chars: ArrayBuffer[Char], nbrs: Seq[Int], i: Int, currBroken: Int): Int = {
-    if (i == chars.size) {
-      if (nbrs.isEmpty && currBroken == 0) {
-        println(s"i $i broken $currBroken $nbrs ${chars.mkString("")}")
-        1
-      } else {
-        if (nbrs.size==1 && nbrs.head == currBroken) {
-          println(s"i $i broken $currBroken $nbrs ${chars.mkString("")}")
+  type Key = (Seq[Int], Int, Int)
+
+  var cache = Map[Key, Long]()
+
+  def nbrArrangements(chars: ArrayBuffer[Char], nbrs: Seq[Int], i: Int, currBroken: Int): Long = {
+    val key = (nbrs, i, currBroken)
+    if (cache.contains(key)) {
+      cache(key)
+    } else {
+      val result: Long = if (nbrs.nonEmpty && nbrs.forall(n => currBroken > n)) {
+        0
+      } else if (i == chars.size) {
+        if (nbrs.isEmpty && currBroken == 0) {
           1
         } else {
-          0
+          if (nbrs.size == 1 && nbrs.head == currBroken) {
+            1
+          } else {
+            0
+          }
+        }
+      } else {
+        chars(i) match {
+          case '.' =>
+            if (currBroken == 0) {
+              nbrArrangements(chars, nbrs, i + 1, currBroken)
+            } else {
+              if (nbrs.nonEmpty && nbrs.head == currBroken) {
+                nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
+              } else {
+                0
+              }
+            }
+          case '#' =>
+            if (currBroken == 0) {
+              nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
+            } else {
+              nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
+            }
+          case '?' =>
+            // chars(i) = '#'
+            val a = if (nbrs.nonEmpty) {
+              if (currBroken == 0) {
+                nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
+              } else {
+                nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
+              }
+            } else {
+              0
+            }
+            // chars(i) = '?'
+
+            // chars(i) = '.'
+            val b = if (currBroken == 0) {
+              nbrArrangements(chars, nbrs, i + 1, currBroken)
+            } else {
+              if (nbrs.nonEmpty && nbrs.head == currBroken) {
+                nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
+              } else {
+                0
+              }
+            }
+            // chars(i) = '?'
+            a + b
         }
       }
-    } else {
-      chars(i) match {
-        case '.' =>
-          if (currBroken == 0) {
-            nbrArrangements(chars, nbrs, i + 1, currBroken)
-          } else {
-            if (nbrs.nonEmpty && nbrs.head == currBroken) {
-              nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
-            } else {
-              0
-            }
-          }
-        case '#' =>
-          nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
-        case '?' =>
-          chars(i) = '#'
-          val a = nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
-          chars(i) = '?'
+      if (cache.contains(key) && cache(key) != result) {
+        println(
+          s"i $i nbrs $nbrs broken $currBroken ${chars.mkString("")} key $key result $result cached ${cache(key)}"
+        )
+      }
+      cache = cache + (key -> result)
+      result
+    }
+  }
 
-          chars(i) = '.'
-          val b = if (currBroken == 0) {
-            nbrArrangements(chars, nbrs, i + 1, currBroken)
-          } else {
-            if (nbrs.nonEmpty && nbrs.head == currBroken) {
-              nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
-            } else {
-              0
-            }
-          }
-          chars(i) = '?'
-          a + b
+  def unfold(charsNumbers: (ArrayBuffer[Char], Seq[Int])): (ArrayBuffer[Char], Seq[Int]) = {
+    val originalChars = charsNumbers.head
+    val chars = ArrayBuffer[Char]()
+    var nbrs = Seq[Int]()
+
+    0.until(5).foreach { i =>
+      chars.addAll(originalChars)
+      if (i < 4) {
+        chars.addOne('?')
       }
     }
+    0.until(5).map { _ =>
+      nbrs = nbrs ++ charsNumbers.last
+    }
+
+    (chars, nbrs)
   }
 
   def parse(line: String): (ArrayBuffer[Char], Seq[Int]) = {
--- src/test/scala/se/johan1a/adventofcode2023/Day12Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day12Test.scala
@@ -5,31 +5,39 @@ import se.johan1a.adventofcode2023.TestInputUtil.getInput
 class Day12Test extends munit.FunSuite {
 
   test("Part 1 test0") {
-    assertEquals(Day12.part1(getInput("day12/test0.txt")), 1)
+    assertEquals(Day12.part1(getInput("day12/test0.txt")), 1L)
   }
 
   test("Part 1 test1") {
-    assertEquals(Day12.part1(getInput("day12/test1.txt")), 4)
+    assertEquals(Day12.part1(getInput("day12/test1.txt")), 4L)
   }
 
   test("Part 1 test2") {
-    assertEquals(Day12.part1(getInput("day12/test2.txt")), 10)
+    assertEquals(Day12.part1(getInput("day12/test2.txt")), 10L)
   }
 
   test("Part 1 test") {
-    assertEquals(Day12.part1(getInput("day12/test.txt")), 21)
+    assertEquals(Day12.part1(getInput("day12/test.txt")), 21L)
   }
 
   test("Part 1") {
-    assertEquals(Day12.part1(getInput("day12/input.txt")), 8270)
+    assertEquals(Day12.part1(getInput("day12/input.txt")), 8270L)
   }
 
-  // test("Part 2 test") {
-  //   assertEquals(Day12.part2(getInput("day12/test.txt")), -1)
-  // }
-  //
-  // test("Part 2") {
-  //   assertEquals(Day12.part2(getInput("day12/input.txt")), -1)
-  // }
+  test("Part 2 test0") {
+    assertEquals(Day12.part2(getInput("day12/test0.txt")), 1L)
+  }
+
+  test("Part 2 test3") {
+    assertEquals(Day12.part2(getInput("day12/test3.txt")), 16L)
+  }
+
+  test("Part 2 test") {
+    assertEquals(Day12.part2(getInput("day12/test.txt")), 525152L)
+  }
+
+  test("Part 2") {
+    assertEquals(Day12.part2(getInput("day12/input.txt")), 204640299929836L)
+  }
 
 }
