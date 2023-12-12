d708106f4a30b372a8c0e63cc22a695223a40ba6
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-12 11:13:54 +0100

    day12 cleanup

--- src/main/scala/se/johan1a/adventofcode2023/Day12.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day12.scala
@@ -15,22 +15,17 @@ object Day12 {
   }
 
   def part2(input: Seq[String]): Long = {
-    println(s"input size ${input.size}")
     input
       .map(parse)
       .map(unfold)
       .map { case (line, nbrs) =>
         cache = Map()
-        val r = nbrArrangements(line, nbrs, 0, 0)
-        println(s"got $r")
-
-        r
+        nbrArrangements(line, nbrs, 0, 0)
       }
       .sum
   }
 
   type Key = (Seq[Int], Int, Int)
-
   var cache = Map[Key, Long]()
 
   def nbrArrangements(chars: ArrayBuffer[Char], nbrs: Seq[Int], i: Int, currBroken: Int): Long = {
@@ -38,89 +33,63 @@ object Day12 {
     if (cache.contains(key)) {
       cache(key)
     } else {
-      val result: Long = if (nbrs.nonEmpty && nbrs.forall(n => currBroken > n)) {
+      val result: Long = if (nbrs.nonEmpty && nbrs.forall(currBroken > _)) {
         0
       } else if (i == chars.size) {
         if (nbrs.isEmpty && currBroken == 0) {
           1
+        } else if (nbrs.size == 1 && nbrs.head == currBroken) {
+          1
         } else {
-          if (nbrs.size == 1 && nbrs.head == currBroken) {
-            1
-          } else {
-            0
-          }
+          0
         }
       } else {
         chars(i) match {
           case '.' =>
             if (currBroken == 0) {
               nbrArrangements(chars, nbrs, i + 1, currBroken)
-            } else {
-              if (nbrs.nonEmpty && nbrs.head == currBroken) {
-                nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
-              } else {
-                0
-              }
-            }
-          case '#' =>
-            if (currBroken == 0) {
-              nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
-            } else {
-              nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
-            }
-          case '?' =>
-            // chars(i) = '#'
-            val a = if (nbrs.nonEmpty) {
-              if (currBroken == 0) {
-                nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
-              } else {
-                nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
-              }
+            } else if (nbrs.nonEmpty && nbrs.head == currBroken) {
+              nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
             } else {
               0
             }
-            // chars(i) = '?'
-
-            // chars(i) = '.'
-            val b = if (currBroken == 0) {
-              nbrArrangements(chars, nbrs, i + 1, currBroken)
+          case '#' =>
+            nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
+          case '?' =>
+            val brokenResult = if (nbrs.nonEmpty) {
+              nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
             } else {
-              if (nbrs.nonEmpty && nbrs.head == currBroken) {
-                nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
-              } else {
-                0
-              }
+              0
             }
-            // chars(i) = '?'
-            a + b
+
+            val notBrokenResult = if (currBroken == 0) {
+              nbrArrangements(chars, nbrs, i + 1, currBroken)
+            } else if (nbrs.nonEmpty && nbrs.head == currBroken) {
+              nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
+            } else {
+              0
+            }
+            brokenResult + notBrokenResult
         }
-      }
-      if (cache.contains(key) && cache(key) != result) {
-        println(
-          s"i $i nbrs $nbrs broken $currBroken ${chars.mkString("")} key $key result $result cached ${cache(key)}"
-        )
       }
       cache = cache + (key -> result)
       result
     }
   }
 
-  def unfold(charsNumbers: (ArrayBuffer[Char], Seq[Int])): (ArrayBuffer[Char], Seq[Int]) = {
-    val originalChars = charsNumbers.head
-    val chars = ArrayBuffer[Char]()
-    var nbrs = Seq[Int]()
-
-    0.until(5).foreach { i =>
-      chars.addAll(originalChars)
-      if (i < 4) {
-        chars.addOne('?')
+  def unfold: ((ArrayBuffer[Char], Seq[Int])) => (ArrayBuffer[Char], Seq[Int]) = { case (chars, nbrs) =>
+    val n = 5
+    val newChars = ArrayBuffer[Char]()
+    0.until(n).foreach { i =>
+      newChars.addAll(chars)
+      if (i < n - 1) {
+        newChars.addOne('?')
       }
     }
-    0.until(5).map { _ =>
-      nbrs = nbrs ++ charsNumbers.last
-    }
 
-    (chars, nbrs)
+    val newNbrs = Seq.fill(n)(nbrs).flatten
+
+    (newChars, newNbrs)
   }
 
   def parse(line: String): (ArrayBuffer[Char], Seq[Int]) = {
