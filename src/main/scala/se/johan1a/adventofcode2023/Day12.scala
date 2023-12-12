6c5729fa485374753a8073aade7fe7b3a1ad37ff
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-12 06:41:54 +0100

    day12 part1

--- src/main/scala/se/johan1a/adventofcode2023/Day12.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day12.scala
@@ -1,12 +1,70 @@
 package se.johan1a.adventofcode2023
 
+import scala.collection.mutable.ArrayBuffer
+
 object Day12 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    println(s"\n")
+    input.map(parse).map { case (line, nbrs) => nbrArrangements(line, nbrs, 0, 0) }.sum
   }
 
   def part2(input: Seq[String]): Int = {
     -1
+  }
+
+  def nbrArrangements(chars: ArrayBuffer[Char], nbrs: Seq[Int], i: Int, currBroken: Int): Int = {
+    if (i == chars.size) {
+      if (nbrs.isEmpty && currBroken == 0) {
+        println(s"i $i broken $currBroken $nbrs ${chars.mkString("")}")
+        1
+      } else {
+        if (nbrs.size==1 && nbrs.head == currBroken) {
+          println(s"i $i broken $currBroken $nbrs ${chars.mkString("")}")
+          1
+        } else {
+          0
+        }
+      }
+    } else {
+      chars(i) match {
+        case '.' =>
+          if (currBroken == 0) {
+            nbrArrangements(chars, nbrs, i + 1, currBroken)
+          } else {
+            if (nbrs.nonEmpty && nbrs.head == currBroken) {
+              nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
+            } else {
+              0
+            }
+          }
+        case '#' =>
+          nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
+        case '?' =>
+          chars(i) = '#'
+          val a = nbrArrangements(chars, nbrs, i + 1, currBroken + 1)
+          chars(i) = '?'
+
+          chars(i) = '.'
+          val b = if (currBroken == 0) {
+            nbrArrangements(chars, nbrs, i + 1, currBroken)
+          } else {
+            if (nbrs.nonEmpty && nbrs.head == currBroken) {
+              nbrArrangements(chars, nbrs.drop(1), i + 1, 0)
+            } else {
+              0
+            }
+          }
+          chars(i) = '?'
+          a + b
+      }
+    }
+  }
+
+  def parse(line: String): (ArrayBuffer[Char], Seq[Int]) = {
+    line match {
+      case s"$chars $numbers" =>
+        (ArrayBuffer().addAll(chars.toCharArray), numbers.split(",").map(_.toInt).toSeq)
+    }
   }
 }
--- src/test/scala/se/johan1a/adventofcode2023/Day12Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day12Test.scala
@@ -4,12 +4,32 @@ import se.johan1a.adventofcode2023.TestInputUtil.getInput
 
 class Day12Test extends munit.FunSuite {
 
-  test("Part 1") {
-    assertEquals(Day12.part1(getInput("day12/input.txt")), -1)
+  test("Part 1 test0") {
+    assertEquals(Day12.part1(getInput("day12/test0.txt")), 1)
   }
 
-  test("Part 2") {
-    assertEquals(Day12.part2(getInput("day12/input.txt")), -1)
+  test("Part 1 test1") {
+    assertEquals(Day12.part1(getInput("day12/test1.txt")), 4)
   }
+
+  test("Part 1 test2") {
+    assertEquals(Day12.part1(getInput("day12/test2.txt")), 10)
+  }
+
+  test("Part 1 test") {
+    assertEquals(Day12.part1(getInput("day12/test.txt")), 21)
+  }
+
+  test("Part 1") {
+    assertEquals(Day12.part1(getInput("day12/input.txt")), 8270)
+  }
+
+  // test("Part 2 test") {
+  //   assertEquals(Day12.part2(getInput("day12/test.txt")), -1)
+  // }
+  //
+  // test("Part 2") {
+  //   assertEquals(Day12.part2(getInput("day12/input.txt")), -1)
+  // }
 
 }
