d9fe4c6b9274bde97c6d6c0cee478e08e541f93a
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-13 17:52:43 +0100

    day13 part 1

--- src/main/scala/se/johan1a/adventofcode2023/Day13.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day13.scala
@@ -1,9 +1,64 @@
 package se.johan1a.adventofcode2023
 
+import se.johan1a.adventofcode2023.Utils._
+import scala.collection.mutable.ArrayBuffer
+
 object Day13 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    split(input).map(makeGrid).map(findMirror).sum
+  }
+
+  def findMirror(grid: ArrayBuffer[ArrayBuffer[Char]]): Int = {
+    val fromTop: Option[Int] = 0.until(grid.size - 1).find { row =>
+      mirrorAtRow(grid, row)
+    }
+    fromTop match {
+      case Some(row) =>
+        println(s"found row $row")
+        100 * (row + 1)
+      case None =>
+        0.until(grid.head.size - 1).find { col =>
+          mirrorAtCol(grid, col)
+        } match {
+          case Some(col) =>
+            println(s"found col $col")
+            col + 1
+          case None => throw new Exception("none found")
+        }
+    }
+  }
+
+  def mirrorAtCol(grid: ArrayBuffer[ArrayBuffer[Char]], col: Int): Boolean = {
+    var col0 = col
+    var col1 = col + 1
+    while (col0 >= 0 && col1 < grid.head.size) {
+      val equal = 0.until(grid.size).forall { row =>
+        grid(row)(col0) == grid(row)(col1)
+      }
+      if (!equal) {
+        return false
+      }
+      col0 -= 1
+      col1 += 1
+    }
+    true
+  }
+
+  def mirrorAtRow(grid: ArrayBuffer[ArrayBuffer[Char]], row: Int): Boolean = {
+    var row0 = row
+    var row1 = row + 1
+    while (row0 >= 0 && row1 < grid.size) {
+      val equal = 0.until(grid.head.size).forall { col =>
+        grid(row0)(col) == grid(row1)(col)
+      }
+      if (!equal) {
+        return false
+      }
+      row0 -= 1
+      row1 += 1
+    }
+    true
   }
 
   def part2(input: Seq[String]): Int = {
--- src/test/scala/se/johan1a/adventofcode2023/Day13Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day13Test.scala
@@ -4,12 +4,16 @@ import se.johan1a.adventofcode2023.TestInputUtil.getInput
 
 class Day13Test extends munit.FunSuite {
 
-  test("Part 1") {
-    assertEquals(Day13.part1(getInput("day13/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day13.part1(getInput("day13/test.txt")), 405)
   }
 
-  test("Part 2") {
-    assertEquals(Day13.part2(getInput("day13/input.txt")), -1)
+  test("Part 1") {
+    assertEquals(Day13.part1(getInput("day13/input.txt")), 30535)
   }
+
+  // test("Part 2") {
+  //   assertEquals(Day13.part2(getInput("day13/input.txt")), -1)
+  // }
 
 }
