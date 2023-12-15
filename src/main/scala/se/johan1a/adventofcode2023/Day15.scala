37fc664a9ba1dba6e4edd973987be8f8fa8d3f25
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-15 09:47:22 +0100

    day15 part 2

--- src/main/scala/se/johan1a/adventofcode2023/Day15.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day15.scala
@@ -3,9 +3,12 @@ package se.johan1a.adventofcode2023
 object Day15 {
 
   def part1(input: Seq[String]): Int = {
-    input.head.split(",").map { str =>
-      hash(str)
-    }.sum
+    input.head
+      .split(",")
+      .map { str =>
+        hash(str)
+      }
+      .sum
   }
 
   def hash(string: String): Int = {
@@ -17,6 +20,43 @@ object Day15 {
   }
 
   def part2(input: Seq[String]): Int = {
-    -1
+    val finalMap = parse(input).foldLeft(Map[Int, Seq[Lens]]()) { case (map, instruction) =>
+      instruction match {
+        case Set(label, n) =>
+          val h = hash(label)
+          val lenses = map.getOrElse(h, Seq.empty)
+          lenses.find(_.label == label) match {
+            case Some(oldLens) =>
+              oldLens.n = n
+              map
+            case None =>
+              map + (h -> (lenses :+ Lens(label, n)))
+          }
+        case Remove(label) =>
+          val h = hash(label)
+          val lenses = map.getOrElse(h, Seq.empty)
+          map + (h -> lenses.filterNot(_.label == label))
+      }
+    }
+    finalMap.map { case (h, lenses) =>
+      lenses.zipWithIndex.map { case (lens, i) =>
+        (h + 1) * (i + 1) * lens.n
+      }.sum
+    }.sum
+  }
+
+  sealed trait Instruction
+  case class Set(label: String, n: Int) extends Instruction
+  case class Remove(label: String) extends Instruction
+
+  case class Lens(label: String, var n: Int)
+
+  def parse(input: Seq[String]) = {
+    input.head.split(",").map { str =>
+      str match {
+        case s"$label=$n" => Set(label, n.toInt)
+        case s"$label-"   => Remove(label)
+      }
+    }
   }
 }
--- src/test/scala/se/johan1a/adventofcode2023/Day15Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day15Test.scala
@@ -9,11 +9,15 @@ class Day15Test extends munit.FunSuite {
   }
 
   test("Part 1") {
-    assertEquals(Day15.part1(getInput("day15/input.txt")), 5077691)
+    assertEquals(Day15.part1(getInput("day15/input.txt")), 507769)
   }
 
-  // test("Part 2") {
-  //   assertEquals(Day15.part2(getInput("day15/input.txt")), -1)
-  // }
+   test("Part 2 test") {
+     assertEquals(Day15.part2(getInput("day15/test.txt")), 145)
+   }
+
+   test("Part 2") {
+    assertEquals(Day15.part2(getInput("day15/input.txt")), 269747)
+  }
 
 }
