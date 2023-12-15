731b49bdd0296f49363d0d5e4560eb0b99dc97bf
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-15 09:10:32 +0100

    day15 part 1

--- src/main/scala/se/johan1a/adventofcode2023/Day15.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day15.scala
@@ -3,7 +3,17 @@ package se.johan1a.adventofcode2023
 object Day15 {
 
   def part1(input: Seq[String]): Int = {
-    -1
+    input.head.split(",").map { str =>
+      hash(str)
+    }.sum
+  }
+
+  def hash(string: String): Int = {
+    string.toCharArray.foldLeft(0) { (acc, char) =>
+      var value = acc
+      value += char.toInt
+      (value * 17) % 256
+    }
   }
 
   def part2(input: Seq[String]): Int = {
--- src/test/scala/se/johan1a/adventofcode2023/Day15Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day15Test.scala
@@ -4,12 +4,16 @@ import se.johan1a.adventofcode2023.TestInputUtil.getInput
 
 class Day15Test extends munit.FunSuite {
 
-  test("Part 1") {
-    assertEquals(Day15.part1(getInput("day15/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day15.part1(Seq("HASH")), 52)
   }
 
-  test("Part 2") {
-    assertEquals(Day15.part2(getInput("day15/input.txt")), -1)
+  test("Part 1") {
+    assertEquals(Day15.part1(getInput("day15/input.txt")), 5077691)
   }
+
+  // test("Part 2") {
+  //   assertEquals(Day15.part2(getInput("day15/input.txt")), -1)
+  // }
 
 }
