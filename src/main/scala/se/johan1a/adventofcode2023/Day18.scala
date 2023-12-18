a7275c0d07bc793ff2efcd5fbde8e3f7b9b5c6bc
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-18 07:42:13 +0100

    day18 part2

--- src/main/scala/se/johan1a/adventofcode2023/Day18.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day18.scala
@@ -4,135 +4,46 @@ import se.johan1a.adventofcode2023.Utils._
 
 object Day18 {
 
-  def part1(input: Seq[String]): Int = {
-    var pos = Vec2(0, 0)
-    var edge = Set(pos)
-    var sideA: Option[Vec2] = None
-    var sideB: Option[Vec2] = None
-    input.map { case s"$dir $n ($color)" => (dir, n.toInt) }.foreach { case (dir, n) =>
-      0.until(n).foreach { _ =>
-        pos = move(pos, dir)
-        edge = edge + pos
-
-        if (n > 4 && sideA.isEmpty) {
-          val left = leftOf(dir)
-          val right = rightOf(dir)
-          sideA = Some(move(pos, left))
-          sideB = Some(move(pos, right))
-        }
-      }
-    }
-
-    // val minY = edge.minBy(_.y)
-    // val maxY = edge.maxBy(_.y)
-    // val minX = edge.minBy(_.x)
-    // val maxX = edge.maxBy(_.x)
-
-    // minY.y.to(maxY.y).foreach { y =>
-    //   minX.x.to(maxX.x).foreach { x=>
-    //     if(edge.contains(Vec2(x,y))) {
-    //       print("#")
-    //     } else {
-    //       print(" ")
-    //     }
-    //   }
-    //   println()
-    // }
-
-    println(edge.size)
-    println(sideA)
-    println(sideB)
-    val inside = flood(edge, sideA.get, sideB.get)
-
-    inside.size + edge.size
+  def part1(input: Seq[String]): Long = {
+    val instructions = input.map { case s"$dir $n ($color)" => (dir, n.toInt) }
+    calculateArea(instructions)
   }
 
   def part2(input: Seq[String]): Long = {
-    var pos = Vec2(0, 0)
-    var edge = Set(pos)
-    var sideA: Option[Vec2] = None
-    var sideB: Option[Vec2] = None
-    input
+    val instructions = input
       .map { case s"$colorA $colorB ($instruction)" =>
-      (Integer.parseInt(instruction.drop(1).take(5), 16), toDir(instruction.takeRight(1)))
+        (toDir(instruction.takeRight(1)), Integer.parseInt(instruction.drop(1).take(5), 16))
       }
-      .foreach { case (n, dir) =>
-        0.until(n).foreach { _ =>
-          pos = move(pos, dir)
-          edge = edge + pos
-
-          if (n > 4 && sideA.isEmpty) {
-            val left = leftOf(dir)
-            val right = rightOf(dir)
-            sideA = Some(move(pos, left))
-            sideB = Some(move(pos, right))
-          }
-        }
-      }
-
-    println(edge.size)
-    println(sideA)
-    println(sideB)
-    val inside = flood(edge, sideA.get, sideB.get)
-
-    inside.size + edge.size
-    edge.size
+    calculateArea(instructions)
   }
 
-  def flood(edge: Set[Vec2], aStart: Vec2, bStart: Vec2) = {
-    var setA = Set[Vec2]()
-    var setB = Set[Vec2]()
-
-    var qA = Set(aStart)
-    var qB = Set(bStart)
-    val maxIter = 10000000
-    var i = 0
-    while (i < maxIter && qA.nonEmpty && qB.nonEmpty) {
-      val a = qA.head
-      qA = qA.tail
-      val b = qB.head
-      qB = qB.tail
-      setA = setA + a
-      setB = setB + b
-      // println(s"setA ${setA.size} qA ${qA.size} setB ${setB.size} qB ${qB.size}")
-
-      qA = qA ++ neighbors(a, includeDiagonals = false).filterNot(p =>
-        edge.contains(p) || setA.contains(p) || setB.contains(p) || qA.contains(p)
-      )
-      qB = qB ++ neighbors(b, includeDiagonals = false).filterNot(p =>
-        edge.contains(p) || setA.contains(p) || setB.contains(p) || qB.contains(p)
-      )
-      i += 1
+  def calculateArea(instructions: Seq[(String, Int)]): Long = {
+    var pos = Vec2(0, 0)
+    var vertices = Seq(pos)
+    var edgeSize = 0L
+    instructions
+      .foreach { case (dir, n) =>
+        val b = move(pos, dir, n)
+        vertices = vertices :+ b
+        pos = b
+        edgeSize += n
+      }
+    var sum = 0L
+    var j = vertices.size - 1
+    0.until(vertices.size).foreach { i =>
+      val a = vertices(j)
+      val b = vertices(i)
+      sum = sum + (a.x + b.x) * (a.y - b.y)
+      j = i
     }
-
-    if(i==maxIter) {
-      println("ERROR maxIter reached")
-    }
-
-    if (qA.isEmpty) {
-      setA
+    val total = if (sum < 0) {
+      -sum
     } else {
-      setB
+      sum
     }
-
-  }
-
-  def leftOf(dir: String) = {
-    dir match {
-      case "U" => "L"
-      case "D" => "R"
-      case "L" => "D"
-      case "R" => "U"
-    }
-  }
-
-  def rightOf(dir: String) = {
-    dir match {
-      case "U" => "R"
-      case "D" => "L"
-      case "L" => "U"
-      case "R" => "D"
-    }
+    println(s"sum $sum total $total")
+    val areaShoelace = total / 2
+    areaShoelace + edgeSize / 2 + 1
   }
 
   def toDir(str: String) = {
--- src/test/scala/se/johan1a/adventofcode2023/Day18Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day18Test.scala
@@ -5,19 +5,19 @@ import se.johan1a.adventofcode2023.TestInputUtil.getInput
 class Day18Test extends munit.FunSuite {
 
   test("Part 1 test") {
-    assertEquals(Day18.part1(getInput("day18/test.txt")), 62)
+    assertEquals(Day18.part1(getInput("day18/test.txt")), 62L)
   }
 
   test("Part 1") {
-    assertEquals(Day18.part1(getInput("day18/input.txt")), 47139)
+    assertEquals(Day18.part1(getInput("day18/input.txt")), 47139L)
   }
 
   test("Part 2 test") {
     assertEquals(Day18.part2(getInput("day18/test.txt")), 952408144115L)
   }
 
-//   test("Part 2") {
-//     assertEquals(Day18.part2(getInput("day18/input.txt")), -1L)
-//   }
+  test("Part 2") {
+    assertEquals(Day18.part2(getInput("day18/input.txt")), 173152345887206L)
+  }
 
 }
