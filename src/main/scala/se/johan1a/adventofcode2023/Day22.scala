93f26c9a60c3e2b571a7ea69d93c95f91e3bb0e2
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-22 23:30:01 +0100

    day22 part1

--- src/main/scala/se/johan1a/adventofcode2023/Day22.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day22.scala
@@ -1,12 +1,124 @@
 package se.johan1a.adventofcode2023
 
+import se.johan1a.adventofcode2023.Utils._
+
 object Day22 {
 
+  case class Brick(start: Vec3, end: Vec3, id: Int = -1)
+
   def part1(input: Seq[String]): Int = {
-    -1
+    val bricks = parse(input)
+    val staticBricks = fall(bricks)
+    println("bricks")
+    staticBricks.foreach(println)
+    // printBricks(staticBricks)
+    val c = count(staticBricks)
+    // println("\nstatic")
+    // staticBricks.takeRight(20).foreach(println)
+    c
+  }
+
+  def count(bricks: Seq[Brick]) = {
+    val supporting: Map[Brick, Seq[Brick]] = bricks.map { brick =>
+      brick -> bricks.filter(b => b.end.z == brick.start.z - 1 && intersectsXY(brick, b))
+    }.toMap
+
+    // supporting.foreach { (k,v)=>
+    //   println(s"$k supports $v")
+    // }
+
+    var supportedBy = Map[Int, Set[Int]]().withDefaultValue(Set.empty)
+
+    val result = bricks.filter { brick =>
+      val bricksAbove = bricks
+        .filter(b => b.start.z == brick.end.z + 1 && intersectsXY(brick, b))
+
+      bricksAbove.map { b =>
+        supportedBy = supportedBy.updated(b.id, supportedBy(b.id) + brick.id)
+      }
+
+      val result = bricksAbove.filter(above => supporting(above).size == 1).isEmpty
+
+      // println(s"\nbricks above $brick:")
+      // bricksAbove.foreach(b => {
+      //   println(s"$b nbr supporting: ${supporting(b).size}")
+      //   println("supported by: ")
+      //   supporting(b).foreach(println)
+      // })
+      // println(s"Can be removed? $result")
+
+      result
+    }.size
+
+    supportedBy.map { (k, v) =>
+      println(s"$k resting on {${v.toSeq.sorted.mkString(", ")}}")
+    }
+
+    result
+  }
+
+  def fall(allBricks: Seq[Brick]) = {
+    var bricks = allBricks
+    var static = Seq[Brick]()
+    var maxHeight = Map[Vec2, Long]().withDefaultValue(0L)
+    var posToId = Map[Vec2, Int]()
+
+    while (bricks.nonEmpty) {
+      val brick = bricks.head
+      bricks = bricks.tail
+      var max = -1L
+      brick.start.x.to(brick.end.x).foreach { x =>
+        brick.start.y.to(brick.end.y).foreach { y =>
+          val h : Long = maxHeight(Vec2(x, y))
+          if (h > max) {
+            max = h
+            maxHeight = maxHeight.updated(Vec2(x, y), h)
+            posToId = posToId.updated(Vec2(x, y), brick.id)
+          }
+        }
+      }
+      val diff = brick.end.z - brick.start.z
+      brick.start.x.to(brick.end.x).foreach { x =>
+        brick.start.y.to(brick.end.y).foreach { y =>
+          maxHeight = maxHeight.updated(Vec2(x, y), max + 1 + diff)
+          posToId = posToId.updated(Vec2(x, y), brick.id)
+        }
+      }
+
+      static = brick.copy(
+        start = brick.start.copy(z = max + 1),
+        end = brick.end.copy(z = max + 1 + diff)
+      ) +: static
+    }
+
+    assert(static.size == allBricks.size)
+    static
+  }
+
+  def intersectsXY(a: Brick, b: Brick): Boolean = {
+    val outside: Boolean = b.start.x > a.end.x || a.start.x > b.end.x || b.start.y > a.end.y || a.start.y > b.end.y
+    // println(s"$a intersects with $b ? ${!outside}")
+    !outside
   }
 
   def part2(input: Seq[String]): Int = {
     -1
+  }
+
+  def parse(input: Seq[String]): Seq[Brick] = {
+    var i = 0
+    input
+      .map { line =>
+        line match {
+          case s"$x0,$y0,$z0~$x1,$y1,$z1" =>
+            val b = Brick(Vec3(x0.toLong, y0.toLong, z0.toLong), Vec3(x1.toLong, y1.toLong, z1.toLong), i)
+            i += 1
+            assert(z0.toLong <= z1.toLong)
+            assert(x0.toLong <= x1.toLong)
+            assert(y0.toLong <= y1.toLong)
+            b
+        }
+      }
+      .sortBy(b => b.start.z)
   }
 }
--- src/test/scala/se/johan1a/adventofcode2023/Day22Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day22Test.scala
@@ -1,15 +1,68 @@
 package se.johan1a.adventofcode2023
 
 import se.johan1a.adventofcode2023.TestInputUtil.getInput
+import se.johan1a.adventofcode2023.Day22._
+import se.johan1a.adventofcode2023.Utils._
 
 class Day22Test extends munit.FunSuite {
 
-  test("Part 1") {
-    assertEquals(Day22.part1(getInput("day22/input.txt")), -1)
+  test("Part 1 test") {
+    assertEquals(Day22.part1(getInput("day22/test.txt")), 5)
   }
 
-  test("Part 2") {
-    assertEquals(Day22.part2(getInput("day22/input.txt")), -1)
+  test("Part 1 test0") {
+    assertEquals(Day22.part1(getInput("day22/test0.txt")), 3)
   }
+
+  test("Part 1 test1") {
+    assertEquals(Day22.part1(getInput("day22/test1.txt")), 2)
+  }
+
+  test("Part 1 test") {
+    assertEquals(Day22.part1(getInput("day22/test2.txt")), 34)
+  }
+
+  test("Part 1") {
+    assertEquals(Day22.part1(getInput("day22/input.txt")), 461)
+  }
+
+  test("intersects") {
+    val a = Brick(Vec3(0, 0, 0), Vec3(1, 1, 1))
+    val b = Brick(Vec3(0, 0, 0), Vec3(1, 1, 1))
+    assertEquals(Day22.intersectsXY(a, b), true)
+  }
+
+  test("intersects") {
+    val a = Brick(Vec3(0, 0, 0), Vec3(1, 1, 1))
+    val b = Brick(Vec3(1, 0, 0), Vec3(2, 1, 1))
+    assertEquals(Day22.intersectsXY(a, b), true)
+  }
+
+  test("intersects") {
+    val a = Brick(Vec3(0, 0, 0), Vec3(1, 1, 1))
+    val b = Brick(Vec3(0, 1, 0), Vec3(1, 2, 1))
+    assertEquals(Day22.intersectsXY(a, b), true)
+  }
+
+  test("intersects") {
+    val a = Brick(Vec3(0, 0, 0), Vec3(1, 1, 1))
+    val b = Brick(Vec3(0, 0, 2), Vec3(1, 1, 4))
+    assertEquals(Day22.intersectsXY(a, b), true)
+  }
+
+  test("fall") {
+    val a = Brick(Vec3(0, 0, 1), Vec3(1, 1, 2))
+    val b = Brick(Vec3(0, 0, 3), Vec3(1, 1, 5))
+
+    val expected = Seq(
+      Brick(Vec3(0, 0, 3), Vec3(1, 1, 5)), a
+    )
+
+    assertEquals(Day22.fall(Seq(a, b)), expected)
+  }
+
+//   test("Part 2") {
+//     assertEquals(Day22.part2(getInput("day22/input.txt")), -1)
+//   }
 
 }
