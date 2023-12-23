43a5c03cfaa6cc0ce88db0a0f777b0cc29fbb537
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-23 08:50:00 +0100

    day23 part2 naive solution, stack overflow

--- src/main/scala/se/johan1a/adventofcode2023/Day23.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day23.scala
@@ -11,32 +11,38 @@ object Day23 {
     longest(grid, Set(start), start, target) - 1
   }
 
-  def longest(grid: Grid, seen: Set[Vec2], pos: Vec2, target: Vec2): Long = {
+  def part2(input: Seq[String]): Long = {
+    println("Start part2")
+    val grid = makeGrid(input)
+    val start = Vec2(1, 0)
+    val target = Vec2(grid.head.size - 2, grid.size - 1)
+    longest(grid, Set(start), start, target, slippery = false) - 1
+  }
+
+  def longest(grid: Grid, seen: Set[Vec2], pos: Vec2, target: Vec2, slippery: Boolean = true): Long = {
     if (pos == target) {
       println(s"at target after ${seen.size} steps")
       seen.size
     } else {
-      val results = getNeighbors(grid, seen, pos).map { neighbor =>
-        longest(grid, seen + neighbor, neighbor, target)
+      val results = getNeighbors(grid, seen, pos, slippery).map { neighbor =>
+        longest(grid, seen + neighbor, neighbor, target, slippery)
       }
-      if(results.nonEmpty) {
+      if (results.nonEmpty) {
         results.max
       } else {
         -1
       }
     }
   }
-  def getNeighbors(grid: Grid, seen: Set[Vec2], pos: Vec2): Seq[Vec2] = {
+
+  def getNeighbors(grid: Grid, seen: Set[Vec2], pos: Vec2, slippery: Boolean): Seq[Vec2] = {
     (get(grid, pos) match {
-      case '>' => Seq(pos + Vec2(1, 0))
-      case '<' => Seq(pos + Vec2(-1, 0))
-      case '^' => Seq(pos + Vec2(0, -1))
-      case 'v' => Seq(pos + Vec2(0, 1))
-      case _ => neighbors(pos, includeDiagonals = false)
+      case '>' if slippery => Seq(pos + Vec2(1, 0))
+      case '<' if slippery => Seq(pos + Vec2(-1, 0))
+      case '^' if slippery => Seq(pos + Vec2(0, -1))
+      case 'v' if slippery => Seq(pos + Vec2(0, 1))
+      case _               => neighbors(pos, includeDiagonals = false)
     }).filter(p => inRange(grid, p) && !(get(grid, p) == '#') && !seen.contains(p))
   }
 
-  def part2(input: Seq[String]): Long = {
-    -1
-  }
 }
