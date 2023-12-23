e24d00f54a4033327c6ea6eef02b8cd195f25164
Johan Andersson <johan.peter.andersson@gmail.com>
2023-12-23 08:44:49 +0100

    day23 part1

--- src/main/scala/se/johan1a/adventofcode2023/Day23.scala
+++ src/main/scala/se/johan1a/adventofcode2023/Day23.scala
@@ -1,12 +1,42 @@
 package se.johan1a.adventofcode2023
 
+import se.johan1a.adventofcode2023.Utils._
+
 object Day23 {
 
-  def part1(input: Seq[String]): Int = {
-    -1
+  def part1(input: Seq[String]): Long = {
+    val grid = makeGrid(input)
+    val start = Vec2(1, 0)
+    val target = Vec2(grid.head.size - 2, grid.size - 1)
+    longest(grid, Set(start), start, target) - 1
   }
 
-  def part2(input: Seq[String]): Int = {
+  def longest(grid: Grid, seen: Set[Vec2], pos: Vec2, target: Vec2): Long = {
+    if (pos == target) {
+      println(s"at target after ${seen.size} steps")
+      seen.size
+    } else {
+      val results = getNeighbors(grid, seen, pos).map { neighbor =>
+        longest(grid, seen + neighbor, neighbor, target)
+      }
+      if(results.nonEmpty) {
+        results.max
+      } else {
+        -1
+      }
+    }
+  }
+  def getNeighbors(grid: Grid, seen: Set[Vec2], pos: Vec2): Seq[Vec2] = {
+    (get(grid, pos) match {
+      case '>' => Seq(pos + Vec2(1, 0))
+      case '<' => Seq(pos + Vec2(-1, 0))
+      case '^' => Seq(pos + Vec2(0, -1))
+      case 'v' => Seq(pos + Vec2(0, 1))
+      case _ => neighbors(pos, includeDiagonals = false)
+    }).filter(p => inRange(grid, p) && !(get(grid, p) == '#') && !seen.contains(p))
+  }
+
+  def part2(input: Seq[String]): Long = {
     -1
   }
 }
--- src/test/scala/se/johan1a/adventofcode2023/Day23Test.scala
+++ src/test/scala/se/johan1a/adventofcode2023/Day23Test.scala
@@ -4,12 +4,20 @@ import se.johan1a.adventofcode2023.TestInputUtil.getInput
 
 class Day23Test extends munit.FunSuite {
 
+  test("Part 1 test0") {
+    assertEquals(Day23.part1(getInput("day23/test0.txt")), 12L)
+  }
+
+  test("Part 1 test") {
+    assertEquals(Day23.part1(getInput("day23/test.txt")), 94L)
+  }
+
   test("Part 1") {
-    assertEquals(Day23.part1(getInput("day23/input.txt")), -1)
+    assertEquals(Day23.part1(getInput("day23/input.txt")), 2094L)
   }
 
   test("Part 2") {
-    assertEquals(Day23.part2(getInput("day23/input.txt")), -1)
+    assertEquals(Day23.part2(getInput("day23/input.txt")), -1L)
   }
 
 }
